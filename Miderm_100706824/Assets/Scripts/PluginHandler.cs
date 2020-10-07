9eaede3fb8ff444ee3cfe7ab74d0ce86ff35b400
Anthony <anthony.ona@ontariotechu.net>
2020-10-07 14:12:17 -0400

    Tutorial DLL added

---
+++ Miderm_100706824/Assets/Scripts/PluginHandler.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Runtime.InteropServices;
+
+public class PluginHandler : MonoBehaviour
+{
+    const string DLL_NAME = "EnginesDLL_Tutorial";
+
+    [DllImport(DLL_NAME)]
+    private static extern void ResetLogger();
+
+    [DllImport(DLL_NAME)]
+    private static extern void SaveCheckPointTime(float RTBC);
+
+    [DllImport(DLL_NAME)]
+    private static extern float GetTotalTime();
+
+    [DllImport(DLL_NAME)]
+    private static extern float GetCheckPointTime(int index);
+
+    [DllImport(DLL_NAME)]
+    private static extern int GetNumCheckpoints();
+
+    float lastTime = 0.0f;
+
+    public void SaveTimeTest(float CPtime)
+    {
+        SaveCheckPointTime(CPtime);
+    }
+
+    public float LoadTimeTest(int index)
+    {
+       if(index >= GetNumCheckpoints())
+       {
+            return -1.0f;
+       }
+       else
+        {
+            return GetCheckPointTime(index);
+        }
+    }
+
+    public float LoadTotalTimeTest()
+    {
+        return GetTotalTime();
+    }
+
+    public void ResetLoggerTest()
+    {
+        ResetLogger();
+    }
+
+    void OnDestroy()
+    {
+        ResetLoggerTest();
+    }
+
+    void Start()
+    {
+        lastTime = Time.time;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            float currentTime = Time.time;
+            float checkpointTime = currentTime - lastTime;
+            lastTime = currentTime;
+
+            SaveTimeTest(checkpointTime);
+        }
+
+        for(int i = 0; i < 10; i++)
+        {
+            if (Input.GetKeyDown(KeyCode.Alpha0+i))
+            {
+                Debug.Log(LoadTimeTest(i));
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            Debug.Log(LoadTotalTimeTest());
+        }
+    }
+}
